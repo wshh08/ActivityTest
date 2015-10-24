0c7041fc3b4f71fb1cf5be76d30cc1d9dbdbd2d5
wshh08 <wshh08@126.com>
2015-10-24 13:45:43 +0800

    second

--- app/src/main/java/com/example/wshh08/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/wshh08/activitytest/FirstActivity.java
@@ -1,29 +1,39 @@
 package com.example.wshh08.activitytest;
 
 // import android.app.Activity;
+
 import android.content.Intent;
-//import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-// import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
-import android.support.v7.widget.Toolbar;
+
+//import android.net.Uri;
+// import android.view.Window;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 //import com.example.test_module.MyClass;
 
 /**
  * Created by wshh08 on 15-10-13.
  */
-public class FirstActivity extends AppCompatActivity{
+public class FirstActivity extends AppCompatActivity {
+    @Bind(R.id.id_toolbar)
+    Toolbar idToolbar;
+    @Bind(R.id.button_1)
+    Button button1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.first_layout);
+        ButterKnife.bind(this);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.id_toolbar);
         // toolbar.setNavigationIcon(R.drawable.s3);
@@ -50,7 +60,7 @@ public class FirstActivity extends AppCompatActivity{
                 //Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
                 //intent.putExtra("extra_data", data);
                 //startActivity(intent);
-                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
                 startActivityForResult(intent, 1);
             }
         });
@@ -80,7 +90,7 @@ public class FirstActivity extends AppCompatActivity{
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.add_item:
-                Toast.makeText(this,"You Clicked Add",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "You Clicked Add", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.second_item:
                 Intent intent = new Intent("com.example.wshh08.activitytest.ACTION_START");
